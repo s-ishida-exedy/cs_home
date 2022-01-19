498a3649c84e9e4dc12bc0217199a247bcb48e79
s-ishida-exedy <s-ishida@exedy.com>
2022-01-20 08:19:46 +0900

    列幅の調整

--- epa_request.aspx.vb
+++ epa_request.aspx.vb
@@ -1,5 +1,5 @@
 ﻿Imports System.Data.SqlClient
-Partial Class yuusen
+Partial Class cs_home
     Inherits System.Web.UI.Page
 
     Public strRow As String
@@ -7,9 +7,28 @@ Partial Class yuusen
 
     Private Sub GridView1_RowCreated(sender As Object, e As System.Web.UI.WebControls.GridViewRowEventArgs) Handles GridView1.RowCreated
 
-        'コード列非表示処理
+        '列非表示処理
         If e.Row.RowType = DataControlRowType.DataRow OrElse e.Row.RowType = DataControlRowType.Header Then
             'e.Row.Cells(10).Visible = False
+        End If
+
+        'ボタンに行数をセット
+        If e.Row.RowType = DataControlRowType.DataRow Then
+            Dim dltButton As Button = e.Row.FindControl("Button1")
+            'ボタンが存在する場合のみセット
+            If Not (dltButton Is Nothing) Then
+                dltButton.CommandArgument = e.Row.RowIndex.ToString()
+            End If
+
+            Dim updButton As Button = e.Row.FindControl("Button2")
+            Dim canButton As Button = e.Row.FindControl("Button3")
+            Dim delButton As Button = e.Row.FindControl("Button4")
+            'ボタンが存在する場合のみセット
+            If Not (updButton Is Nothing) Then
+                updButton.CommandArgument = e.Row.RowIndex.ToString()
+                canButton.CommandArgument = e.Row.RowIndex.ToString()
+                delButton.CommandArgument = e.Row.RowIndex.ToString()
+            End If
         End If
 
     End Sub
@@ -61,8 +80,48 @@ Partial Class yuusen
         Me.Label2.Text = Left(strDate, 4) & "/" & Mid(strDate, 5, 2) & "/" & Mid(strDate, 7, 2) _
              & " " & Mid(strDate, 9, 2) & ":" & Mid(strDate, 11, 2) & ":" & Mid(strDate, 13, 2) & " 更新"
 
+        AddHandler GridView1.RowCommand, AddressOf GridView1_RowCommand
     End Sub
 
+    Private Sub GridView1_RowEditing(sender As Object, e As GridViewEditEventArgs) Handles GridView1.RowEditing
+        '編集ボタン押下
+        '選択行のステータスを取得する。
+        'Dim sch_ID As String = GridView1.SelectedValue.ToString
+        ''選択された行のSCH_ID
+        'strRow = sch_ID
 
 
+    End Sub
+
+    Private Sub GridView1_RowCommand(sender As Object, e As GridViewCommandEventArgs) Handles GridView1.RowCommand
+        If e.CommandName = "Edit" Then
+            Dim index As Integer = Convert.ToInt32(e.CommandArgument)
+            'Dim data1 = Me.GridView1.Rows(index).Cells(1).Text
+            Dim row As GridViewRow = GridView1.Rows(index)
+
+
+
+
+
+            'Me.GridView1.Rows(index).Cells(2).Text = Server.HtmlDecode(row.Cells(1).Text)
+
+            'Dim row As GridViewRow = GridView1.Rows(index)
+            'Dim item As New ListItem()
+            'item.Text = Server.HtmlDecode(row.Cells(2).Text)
+            'If Not CustomersListBox.Items.Contains(item) Then
+            '    CustomersListBox.Items.Add(item)
+            'End If
+        End If
+    End Sub
+
+    Protected Sub DropDownList1_SelectedIndexChanged(sender As Object, e As EventArgs)
+        'CType(GridView1.Rows(e.RowIndex).FindControl("DropDownList1"), DropDownList)
+        Dim list As DropDownList = sender
+        list.SelectedValue = "09"
+    End Sub
+
+    Private Sub GridView1_RowUpdating(sender As Object, e As GridViewUpdateEventArgs) Handles GridView1.RowUpdating
+        Dim list As DropDownList = CType(GridView1.Rows(e.RowIndex).FindControl("DropDownList1"), DropDownList)
+        list.SelectedValue = "09"
+    End Sub
 End Class
