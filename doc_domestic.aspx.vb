50e2003787dd2e0878b49bdcf2cdd7430fd4c408
s-ishida-exedy <s-ishida@exedy.com>
2022-02-02 14:01:05 +0900

    手順書画面追加（海外、国内）

---
+++ doc_domestic.aspx.vb
@@ -0,0 +1,86 @@
+﻿Imports System.Data.SqlClient
+Partial Class cs_home
+    Inherits System.Web.UI.Page
+
+    Public strMainPath As String = ""       'サーバーのパス用
+
+    Private Sub form1_Load(sender As Object, e As EventArgs) Handles form1.Load
+        Dim TableRow As TableRow
+        Dim TableCell As TableCell
+        Dim Linkbtn As New LinkButton
+
+        '手順書マスタからデータ取得し、その数の<TD>を作成する。
+        Dim dataread As SqlDataReader
+        Dim dbcmd As SqlCommand
+        Dim strSQL As String = ""
+        Dim intCnt As Integer = 1
+        Dim intAll As Integer = 0
+
+        '接続文字列の作成
+        Dim ConnectionString As String = String.Empty
+        'SQL Server認証
+        ConnectionString = "Data Source=kbhwpm02;Initial Catalog=EXPDB;User Id=sa;Password=expdb-manager"
+        'SqlConnectionクラスの新しいインスタンスを初期化
+        Dim cnn = New SqlConnection(ConnectionString)
+
+        'データベース接続を開く
+        cnn.Open()
+
+        strSQL = strSQL & "SELECT CODE, MANUAL_NM, MANUAL_FILE, PLACE FROM M_EXL_MANUAL_CS "
+        strSQL = strSQL & "WHERE PLACE IN ('0','9') ORDER BY CODE "
+
+        'ＳＱＬコマンド作成 
+        dbcmd = New SqlCommand(strSQL, cnn)
+        'ＳＱＬ文実行 
+        dataread = dbcmd.ExecuteReader()
+
+        '結果を取り出す 
+        While (dataread.Read())
+            If dataread("PLACE") = "9" Then     'サーバーパス
+                strMainPath = dataread("MANUAL_FILE")
+            ElseIf dataread("PLACE") = "0" Then '各ファイル
+                If intCnt = 1 Or intCnt Mod 3 = 1 Then
+                    '次の行を追加
+                    TableRow = New TableRow()
+                End If
+
+                TableCell = New TableCell()
+                TableRow.Cells.Add(TableCell)
+                Linkbtn = New LinkButton
+                Linkbtn.Text = dataread("MANUAL_NM")
+                Linkbtn.ID = dataread("MANUAL_FILE")
+                AddHandler Linkbtn.Click, AddressOf LBtn_Click
+                TableCell.Controls.Add(Linkbtn)
+
+                '３列ごとに行追加
+                If intCnt Mod 3 = 0 Then
+                    '次の行を追加
+                    Table1.Rows.Add(TableRow)
+                End If
+                intCnt += 1
+            End If
+        End While
+
+        '３列に収まらなかった場合、残りの列を追加
+        If intCnt - 1 Mod 3 <> 0 Then
+            Table1.Rows.Add(TableRow)
+        End If
+
+        'クローズ処理 
+        dataread.Close()
+        dbcmd.Dispose()
+        cnn.Close()
+        cnn.Dispose()
+    End Sub
+
+    Private Sub LBtn_Click(sender As Object, e As EventArgs)
+        'クリックされたリンクボタンを取得
+        Dim lnkbutton = CType(sender, LinkButton)
+
+        'ID（ファイル名）を引数にファイルオープン処理
+        Dim p As New System.Diagnostics.Process
+        p.StartInfo.FileName = strMainPath & lnkbutton.ID   'フルパス指定
+        p.Start()
+    End Sub
+
+End Class
