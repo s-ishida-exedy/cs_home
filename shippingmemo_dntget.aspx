a6a3ba5fb9e34776a01c5f1f64383843ba11c89a
exedy-fukao <r-fukao@exedy.com>
2022-02-09 17:44:36 +0900

    test

---
+++ shippingmemo_dntget.aspx
@@ -0,0 +1,290 @@
+﻿<%@ Page Language="VB" AutoEventWireup="false" CodeFile="shippingmemo_dntget.aspx.vb" Inherits="yuusen" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>BL未回収</title>
+
+<meta name="viewport" content="width=device-width, initial-scale=1"/>
+<link rel="stylesheet" href="css/style.css"/>
+<script src="js/openclose.js"></script>
+<script src="js/fixmenu.js"></script>
+<script src="js/fixmenu_pagetop.js"></script>
+<script src="js/ddmenu_min.js"></script>
+<script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+<script src="js/default.js"></script>
+
+
+
+    <style type="text/css">
+         #form1
+        {
+            background-color : #ffffff;
+            color : #000000;
+        }
+        body {
+            background-color: #ffffff;
+        }
+        A.sample1:link { color: blue;}
+        A.sample1:visited { color: blue;}
+        A.sample1:active { color: blue;}
+        A.sample1:hover { color: blue;}
+        .auto-style6 {
+            margin-right: 7px;
+        }
+
+        table{
+          width: 100%;
+        }
+        th {
+          position: sticky;
+          top: 0;
+          z-index: 0;
+          background-color: #FFFFFF;
+          color: #000000;
+        }
+        .wrapper {
+          overflow: scroll;
+          height: 450px;
+        }
+
+ 
+h2 {
+  padding-left: 45px;
+  position: relative;
+  border-radius: 10px; /* 角を丸くする */
+}
+h2:before {
+  content: "";
+  background-color: #6fbfd1;
+  border-radius: 50%;
+  opacity: 0.5;
+  width: 35px;
+  height: 35px;
+  left: 5px;
+  top: 0px;
+  position: absolute;
+}
+h2:after{
+  content: "";
+  background-color: #6fbfd1;
+  border-radius: 50%;
+  opacity: 0.5;
+  width: 20px;
+  height: 20px;
+  left: 25px;
+  top:15px;
+  position: absolute;
+}
+
+
+
+
+
+.button04 a {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 0 auto;
+  padding: 0.5em 1em;
+  width: 100px;
+  color: #000000;
+  font-size: 12px;
+  font-weight: 200;
+  border: 2px solid #ffffff;
+  border-radius: 4px;
+  text-decoration: none;
+  transition: all 0.1s;
+}
+
+.button04 a::after {
+  content: '';
+  width: 5px;
+  height: 5px;
+  border-top: 2px solid #000000;
+  border-right: 2px solid #000000;
+  transform: rotate(45deg);
+
+}
+
+.button04 a:hover {
+  color: #000000;
+  text-decoration: none;
+  background-color: #ffffff;
+  border: 2px solid #000000;
+
+
+}
+
+.button04 a:hover::after {
+  border-top: 2px solid #000000;
+  border-right: 2px solid #000000;
+}
+
+
+    </style>
+<script>
+    $(document).ready(function () {
+        var text = getParam('id');
+        $('.result').text(text);
+        if (text !== 'home') {
+            $('iframe').attr('src', text);
+            return false;   // hrefのクリックイベントキャンセル
+        };
+    });
+    $(function () {
+        $('ul li').off("click").on("click", function () {
+            var arr = $(this).data('id');
+            //var text = $(this).data('id');
+            if (arr[0] == 'home' || arr[0] == 'aaa' || arr[0] == 'undefined') {
+                if (arr[0] == 'home') {
+                    window.location.href = encodeURIComponent(arr[1]);
+                    return false;
+                } else {
+                    return false;
+                }
+            } else{
+                //window.alert('koko3');
+                window.location.href = './detail.aspx?id=' + encodeURIComponent(arr[1]);
+                return false;
+            };
+        });
+    });
+</script>
+</head>
+
+<body class="c2">
+
+<form id="form1" runat="server">
+
+<!--PC用（901px以上端末）メニュー-->
+<!-- インクルードファイルの指定 -->
+<!-- メニューの編集はheader.htmlで行う -->
+ <!-- #Include File="header.html" -->
+
+
+
+<div id="contents2" class="inner2">
+
+    <table style="height:10px;">
+
+        <tr>
+
+
+
+            <td style="width:250px;Font-Size:25px;" >
+
+                <h2>BL未回収</h2>
+
+            </td>
+
+            <td style="width:500px;" >
+            </td>
+
+            <td style="width:80px;" >
+
+                <div class="button04">
+                <a href="shippingmemo.aspx?id={0}">シッピングメモ</a>
+                </div>
+
+            </td>
+
+            <td style="width:80px;" >
+
+
+
+            </td>
+
+        </tr>
+
+    </table>
+
+
+
+
+<asp:Panel ID="Panel1" runat="server"  Font-Size="12px">
+
+<div class="wrapper">
+<table class="sticky">
+<thead class="fixed">
+
+</thead>
+
+
+<tbody>
+
+<asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" DataSourceID="SqlDataSource1" Width="2000px" BackColor="White" BorderColor="#555555" BorderStyle="None" BorderWidth="3px" CellPadding="3" >
+
+
+<HeaderStyle BackColor="#326DB6" Font-Bold="True" ForeColor="BLACK"> </HeaderStyle>
+<HeaderStyle CssClass="Freezing"></HeaderStyle>
+
+<Columns>
+    
+                    <asp:BoundField DataField="CUSTCODE" HeaderText="客先コード" SortExpression="CUSTCODE" ReadOnly="true" ></asp:BoundField>
+                    <asp:BoundField DataField="CUSTNAME" HeaderText="客先名or海貨業者" SortExpression="CUSTNAME" ReadOnly="true" ></asp:BoundField>
+                    <asp:BoundField DataField="INVOICE_NO" HeaderText="IVNO" SortExpression="INVOICE_NO" ReadOnly="true" ></asp:BoundField>
+                    <asp:BoundField DataField="ETD" HeaderText="ETD" SortExpression="ETD" ReadOnly="true" ></asp:BoundField>
+                    <asp:BoundField DataField="REV_ETD" HeaderText="REV_ETD" SortExpression="REV_ETD" ReadOnly="true" ></asp:BoundField>
+
+                    <asp:BoundField DataField="ETA" HeaderText="ETA" SortExpression="ETA" ReadOnly="true" ></asp:BoundField>
+                    <asp:BoundField DataField="REV_ETA" HeaderText="REV_ETA" SortExpression="REV_ETA" ReadOnly="true" ></asp:BoundField>
+
+                    <%--            <asp:BoundField DataField="MEMOFLG" HeaderText="メモ出力" SortExpression="MEMOFLG" ReadOnly="true" ></asp:BoundField>
+                    <asp:BoundField DataField="SIFLG" HeaderText="SI出力" SortExpression="SIFLG" ReadOnly="true" ></asp:BoundField>--%>
+
+                    <asp:BoundField DataField="SHIP_TYPE" HeaderText="種類" SortExpression="SHIP_TYPE" ReadOnly="true" ></asp:BoundField>
+                    <asp:BoundField DataField="DATE_GETBL" HeaderText="BL回収" SortExpression="DATE_GETBL" ></asp:BoundField>
+                    <asp:BoundField DataField="DATE_ONBL" HeaderText="BL上の日付" SortExpression="DATE_ONBL" ></asp:BoundField>
+
+                    <asp:BoundField DataField="REV_SALESDATE" HeaderText="修正後計上日" SortExpression="REV_SALESDATE" ></asp:BoundField>
+                    <asp:BoundField DataField="REV_STATUS" HeaderText="修正状況" SortExpression="REV_STATUS" ></asp:BoundField>
+                    <asp:BoundField DataField="BOOKING_NO" HeaderText="BOOKING_NO" SortExpression="BOOKING_NO" ReadOnly="true" ></asp:BoundField>
+                    <asp:BoundField DataField="VOY_NO" HeaderText="VOY_NO" SortExpression="VOY_NO" ReadOnly="true" ></asp:BoundField>
+                    <asp:BoundField DataField="IV_BLDATE" HeaderText="IV_BLDATE" SortExpression="IV_BLDATE" ReadOnly="true" ></asp:BoundField>
+                    <asp:BoundField DataField="KIN_GAIKA" HeaderText="金額（外貨）" SortExpression="KIN_GAIKA" ReadOnly="true" ></asp:BoundField>
+                    <asp:BoundField DataField="RATE" HeaderText="レート" SortExpression="RATE" ReadOnly="true" ></asp:BoundField>
+                    <asp:BoundField DataField="KIN_JPY" HeaderText="金額（JPY）" SortExpression="KIN_JPY" ReadOnly="true" ></asp:BoundField>
+                    <asp:BoundField DataField="VESSEL" HeaderText="船名" SortExpression="VESSEL" ReadOnly="true" ></asp:BoundField>
+                    <asp:BoundField DataField="LOADING_PORT" HeaderText="積出" SortExpression="LOADING_PORT" ReadOnly="true" ></asp:BoundField>
+                    <asp:BoundField DataField="RECEIVED_PORT" HeaderText="荷受" SortExpression="RECEIVED_PORT" ReadOnly="true" ></asp:BoundField>
+                    <asp:BoundField DataField="SHIP_PLACE" HeaderText="出荷拠点" SortExpression="SHIP_PLACE" ReadOnly="true" ></asp:BoundField>
+                    <asp:BoundField DataField="CHECKFLG" HeaderText="確認" SortExpression="CHECKFLG" ReadOnly="true" ></asp:BoundField>
+
+                     <asp:BoundField DataField="FLG01" HeaderText="メモ" SortExpression="FLG01" ReadOnly="true" ></asp:BoundField>
+<%--                    <asp:BoundField DataField="FLG02" HeaderText="FLG02" SortExpression="FLG02" ReadOnly="true" ></asp:BoundField>
+                    <asp:BoundField DataField="FLG03" HeaderText="FLG03" SortExpression="FLG03" ReadOnly="true" ></asp:BoundField>
+                    <asp:BoundField DataField="FLG04" HeaderText="FLG04" SortExpression="FLG04" ReadOnly="true" ></asp:BoundField>
+                    <asp:BoundField DataField="FLG05" HeaderText="FLG05" SortExpression="FLG05" ReadOnly="true" ></asp:BoundField>--%>
+
+</Columns>
+<FooterStyle BackColor="#CCCCCC" ForeColor="Black" />
+<PagerStyle BackColor="#999999" ForeColor="Black" HorizontalAlign="Center" />
+<RowStyle BackColor="#FFFFFF" ForeColor="Black" />
+<SelectedRowStyle BackColor="#008A8C" Font-Bold="True" ForeColor="White" />
+
+</asp:GridView>
+
+
+</tbody>
+</table>
+
+
+</div>
+</asp:Panel>   
+</div>
+
+<asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:EXPDBConnectionString %>" SelectCommand="SELECT * FROM [T_EXL_SHIPPINGMEMOLIST] WHERE DATE_GETBL='' AND CUSTCODE not in ('B494','B490','B491','B492','B520','A063','A064','A060','A061','A062','B530') ORDER BY ETD "></asp:SqlDataSource>
+
+
+<!--/#contents2-->
+
+<!--ページの上部に戻る「↑」ボタン-->
+<p class="nav-fix-pos-pagetop"><a href="#">↑</a></p>
+
+</form>
+
+</body>
+</html>
---
+++ shippingmemo_dntget.aspx.vb
@@ -0,0 +1,347 @@
+﻿
+Imports System.Data
+Imports System.Data.SqlClient
+Imports System.Data.Common
+Partial Class yuusen
+    Inherits System.Web.UI.Page
+
+    Public strRow As String
+    Public strProcess As String
+
+
+
+
+
+
+    Private Sub GridView1_RowCreated(sender As Object, e As System.Web.UI.WebControls.GridViewRowEventArgs) Handles GridView1.RowDataBound
+
+
+        Dim dataread As SqlDataReader
+        Dim dbcmd As SqlCommand
+        Dim strSQL As String
+
+        Dim strkaika As String
+
+        Dim str01 As String = ""
+        Dim str02 As String = ""
+        Dim str03 As String = ""
+        Dim str04 As String = ""
+        Dim str05 As String = ""
+
+
+
+        If e.Row.RowType = DataControlRowType.DataRow Then
+            If e.Row.Cells(11).Text = "月またぎ" Then
+                e.Row.BackColor = Drawing.Color.DarkSalmon
+            ElseIf e.Row.Cells(11).Text = "出港済み" Then
+                e.Row.BackColor = Drawing.Color.LightBlue
+            End If
+
+            If e.Row.Cells(8).Text = "" Or e.Row.Cells(8).Text = "&nbsp;" Then
+                Call GET_IVDATA(Trim(e.Row.Cells(12).Text))
+            End If
+
+            Dim dt0 As DateTime = DateTime.Parse(e.Row.Cells(3).Text)
+
+            If e.Row.Cells(8).Text <> "&nbsp;" And e.Row.Cells(11).Text = "&nbsp;" Then
+                If e.Row.Cells(3).Text <> "&nbsp;" And e.Row.Cells(9).Text <> "&nbsp;" Then
+
+                    Dim dt1 As DateTime = DateTime.Parse(e.Row.Cells(9).Text)
+
+                    If dt0.ToString("MM") = dt1.ToString("MM") Then
+                        str02 = "不要"
+                        str01 = "-"
+                        Call UPD_MEMO02(Trim(e.Row.Cells(12).Text), str01, str02)
+                    Else
+                        str02 = "要"
+                        str01 = "確認要"
+                        Call UPD_MEMO02(Trim(e.Row.Cells(12).Text), str01, str02)
+                    End If
+                End If
+            End If
+
+
+
+            If e.Row.Cells(14).Text <> "&nbsp;" Then
+
+                If Trim(e.Row.Cells(14).Text) <> "" Then
+                    Dim dt2 As DateTime = DateTime.Parse(e.Row.Cells(14).Text)
+
+                    If dt0.ToString("MM") = dt2.ToString("MM") Then
+                        str03 = "○"
+                        Call UPD_MEMO03(Trim(e.Row.Cells(12).Text), str03)
+                    Else
+                        str03 = "×"
+                        Call UPD_MEMO03(Trim(e.Row.Cells(12).Text), str03)
+                    End If
+                End If
+            End If
+
+            If e.Row.Cells(4).Text <> "&nbsp;" Then
+                If Trim(e.Row.Cells(4).Text) <> "" Then
+
+                    Dim dt3 As DateTime = DateTime.Parse(e.Row.Cells(4).Text)
+
+                    If dt0.ToString("MM") = dt3.ToString("MM") Then
+                        str04 = "○"
+                        Call UPD_MEMO03(Trim(e.Row.Cells(12).Text), str04)
+                    Else
+                        str04 = "×"
+                        Call UPD_MEMO03(Trim(e.Row.Cells(12).Text), str04)
+                    End If
+                End If
+            End If
+
+            Dim dt00 As DateTime = DateTime.Now
+            Dim ts1 As New TimeSpan(7, 0, 0, 0)
+            Dim dt01 As DateTime = dt00 + ts1
+
+            If e.Row.Cells(8).Text = "&nbsp;" Or Trim(e.Row.Cells(8).Text) = "" Then
+                If e.Row.Cells(6).Text = "&nbsp;" Or Trim(e.Row.Cells(6).Text) = "" Then
+
+                    Dim dt4 As DateTime = DateTime.Parse(e.Row.Cells(5).Text)
+
+                    If dt4 <= dt01 Then
+                        e.Row.BackColor = Drawing.Color.Red
+                        e.Row.Cells(23).Text = "E"
+                    Else
+                    End If
+                Else
+
+                    Dim dt3 As DateTime = DateTime.Parse(e.Row.Cells(6).Text)
+
+                    If dt3 <= dt01 Then
+                        e.Row.BackColor = Drawing.Color.Red
+                        e.Row.Cells(23).Text = "E"
+                    Else
+                    End If
+                End If
+            End If
+
+
+
+            '接続文字列の作成
+            Dim ConnectionString As String = String.Empty
+            'SQL Server認証
+            ConnectionString = "Data Source=kbhwpm02;Initial Catalog=EXPDB;User Id=sa;Password=expdb-manager"
+            'SqlConnectionクラスの新しいインスタンスを初期化
+            Dim cnn = New SqlConnection(ConnectionString)
+            Dim Command = cnn.CreateCommand
+
+            'データベース接続を開く
+            cnn.Open()
+
+            strSQL = "SELECT T_BOOKING.Forwarder, T_BOOKING.BOOKING_NO FROM T_BOOKING WHERE T_BOOKING.BOOKING_NO = '" & Trim(e.Row.Cells(12).Text) & "' "
+
+
+            'ＳＱＬコマンド作成
+            dbcmd = New SqlCommand(strSQL, cnn)
+            'ＳＱＬ文実行
+            dataread = dbcmd.ExecuteReader()
+            strkaika = ""
+            '結果を取り出す
+            While (dataread.Read())
+                strkaika += dataread("Forwarder")
+                e.Row.Cells(1).Text = strkaika
+            End While
+
+            'クローズ処理
+            dataread.Close()
+            dbcmd.Dispose()
+
+            cnn.Close()
+            cnn.Dispose()
+
+
+        End If
+
+
+
+
+    End Sub
+
+
+    Private Sub GET_IVDATA(bkgno As String)
+
+        Dim dataread As SqlDataReader
+        Dim dbcmd As SqlCommand
+        Dim strSQL As String = ""
+        Dim strDate As String
+        Dim str01 As String
+        Dim str02 As String
+        Dim str03 As String
+        Dim str04 As String
+        Dim str05 As String
+        Dim str06 As String
+        Dim str07 As String
+        Dim str08 As String
+        Dim str09 As String
+        Dim str10 As String
+
+        '接続文字列の作成
+        Dim ConnectionString As String = String.Empty
+        'SQL Server認証
+        ConnectionString = "Data Source=svdpo051;Initial Catalog=BPTB001;User Id=ado_bptb001;Password=ado_bptb001"
+        'SqlConnectionクラスの新しいインスタンスを初期化
+        Dim cnn = New SqlConnection(ConnectionString)
+
+        'データベース接続を開く
+        cnn.Open()
+
+        strSQL = "SELECT T_INV_HD_TB.OLD_INVNO,T_INV_HD_TB.INVFROM,T_INV_HD_TB.INVON, T_INV_HD_TB.BLDATE, Sum(T_INV_BD_TB.KIN) AS KINの合計,T_INV_HD_TB.INVOICENO,T_INV_HD_TB.STAMP,T_INV_HD_TB.RATE,(Sum(T_INV_BD_TB.KIN) * T_INV_HD_TB.RATE) as JPY,T_INV_HD_TB.BOOKINGNO,T_INV_HD_TB.SHIPPEDPER,T_INV_HD_TB.SHIPBASE,T_INV_HD_TB.VOYAGENO "
+        strSQL = strSQL & "FROM T_INV_BD_TB RIGHT JOIN T_INV_HD_TB ON T_INV_BD_TB.INVOICENO = T_INV_HD_TB.INVOICENO "
+        strSQL = strSQL & "WHERE "
+        strSQL = strSQL & " T_INV_HD_TB.BOOKINGNO is not null "
+        strSQL = strSQL & "GROUP BY T_INV_HD_TB.OLD_INVNO, T_INV_HD_TB.BLDATE,T_INV_HD_TB.INVOICENO,T_INV_HD_TB.STAMP,T_INV_HD_TB.RATE,T_INV_HD_TB.BOOKINGNO,T_INV_HD_TB.SHIPPEDPER,T_INV_HD_TB.SHIPBASE,T_INV_HD_TB.INVFROM,T_INV_HD_TB.INVON,T_INV_HD_TB.VOYAGENO "
+        strSQL = strSQL & "HAVING (((T_INV_HD_TB.BOOKINGNO) = '" & bkgno & "')) "
+        strSQL = strSQL & "AND ((Sum(T_INV_BD_TB.KIN))>0 ) "
+        strSQL = strSQL & "AND T_INV_HD_TB.STAMP = (SELECT MAX(T_INV_HD_TB.STAMP) T_INV_HD_TB WHERE T_INV_HD_TB.BOOKINGNO = '" & bkgno & "') "
+        strSQL = strSQL & "order by T_INV_HD_TB.STAMP DESC "
+
+        'ＳＱＬコマンド作成 
+        dbcmd = New SqlCommand(strSQL, cnn)
+        'ＳＱＬ文実行 
+        dataread = dbcmd.ExecuteReader()
+
+        strDate = ""
+        '結果を取り出す 
+        While (dataread.Read())
+
+            str01 = Convert.ToString(dataread("BOOKINGNO"))        'ETD(計上日)
+            str02 = Convert.ToString(dataread("VOYAGENO"))        'ETD(計上日)
+            str03 = Convert.ToString(dataread("BLDATE"))        'ETD(計上日)
+            str04 = Convert.ToString(dataread("KINの合計"))        'ETD(計上日)
+            str05 = Convert.ToString(dataread("Rate"))        'ETD(計上日)
+            str06 = Convert.ToString(dataread("JPY"))        'ETD(計上日)
+            str07 = Convert.ToString(dataread("SHIPPEDPER"))        'ETD(計上日)
+            str08 = Convert.ToString(dataread("INVFROM"))        'ETD(計上日)
+            str09 = Convert.ToString(dataread("INVON"))        'ETD(計上日)
+            str10 = Convert.ToString(dataread("SHIPBASE"))        'ETD(計上日)
+
+            Call UPD_MEMO(bkgno, str01, str02, str03, str04, str05, str06, str07, str08, str09, str10)
+
+        End While
+
+        'クローズ処理 
+        dataread.Close()
+        dbcmd.Dispose()
+        cnn.Close()
+        cnn.Dispose()
+
+    End Sub
+
+    Private Sub UPD_MEMO(bkgno As String, str01 As String, str02 As String, str03 As String, str04 As String, str05 As String, str06 As String, str07 As String, str08 As String, str09 As String, str10 As String)
+        '接続文字列の作成
+        Dim ConnectionString As String = String.Empty
+        'SQL Server認証
+        ConnectionString = "Data Source=KBHWPM02;Initial Catalog=EXPDB;User Id=sa;Password=expdb-manager"
+        'SqlConnectionクラスの新しいインスタンスを初期化
+        Dim cnn = New SqlConnection(ConnectionString)
+        Dim Command = cnn.CreateCommand
+        Dim strSQL As String = ""
+        Dim ivno As String = ""
+        Dim dataread As SqlDataReader
+        Dim dbcmd As SqlCommand
+
+        Dim dataread2 As SqlDataReader
+        Dim dbcmd2 As SqlCommand
+
+        Dim intCnt As Long
+
+        Dim dt1 As DateTime = DateTime.Now
+
+        'データベース接続を開く
+        cnn.Open()
+
+        strSQL = ""
+        strSQL = strSQL & "UPDATE T_EXL_SHIPPINGMEMOLIST SET "
+        strSQL = strSQL & "T_EXL_SHIPPINGMEMOLIST.BOOKING_NO ='" & str01 & "', "
+        strSQL = strSQL & "T_EXL_SHIPPINGMEMOLIST.VOY_NO ='" & str02 & "', "
+        strSQL = strSQL & "T_EXL_SHIPPINGMEMOLIST.IV_BLDATE ='" & str03 & "', "
+        strSQL = strSQL & "T_EXL_SHIPPINGMEMOLIST.KIN_GAIKA ='" & str04 & "', "
+        strSQL = strSQL & "T_EXL_SHIPPINGMEMOLIST.RATE ='" & str05 & "', "
+        strSQL = strSQL & "T_EXL_SHIPPINGMEMOLIST.KIN_JPY ='" & str06 & "', "
+        strSQL = strSQL & "T_EXL_SHIPPINGMEMOLIST.VESSEL ='" & str07 & "', "
+        strSQL = strSQL & "T_EXL_SHIPPINGMEMOLIST.LOADING_PORT ='" & str08 & "', "
+        strSQL = strSQL & "T_EXL_SHIPPINGMEMOLIST.RECEIVED_PORT ='" & str09 & "', "
+        strSQL = strSQL & "T_EXL_SHIPPINGMEMOLIST.SHIP_PLACE ='" & str10 & "' "
+        strSQL = strSQL & "WHERE T_EXL_SHIPPINGMEMOLIST.BOOKING_NO ='" & bkgno & "' "
+
+        Command.CommandText = strSQL
+        ' SQLの実行
+        Command.ExecuteNonQuery()
+
+        cnn.Close()
+        cnn.Dispose()
+
+    End Sub
+
+    Private Sub UPD_MEMO02(bkgno As String, str01 As String, str02 As String)
+        '接続文字列の作成
+        Dim ConnectionString As String = String.Empty
+        'SQL Server認証
+        ConnectionString = "Data Source=KBHWPM02;Initial Catalog=EXPDB;User Id=sa;Password=expdb-manager"
+        'SqlConnectionクラスの新しいインスタンスを初期化
+        Dim cnn = New SqlConnection(ConnectionString)
+        Dim Command = cnn.CreateCommand
+        Dim strSQL As String = ""
+        Dim ivno As String = ""
+        Dim dataread As SqlDataReader
+        Dim dbcmd As SqlCommand
+        Dim dataread2 As SqlDataReader
+        Dim dbcmd2 As SqlCommand
+        Dim intCnt As Long
+        Dim dt1 As DateTime = DateTime.Now
+
+        'データベース接続を開く
+        cnn.Open()
+
+        strSQL = ""
+        strSQL = strSQL & "UPDATE T_EXL_SHIPPINGMEMOLIST SET "
+        strSQL = strSQL & "T_EXL_SHIPPINGMEMOLIST.REV_SALESDATE ='" & str01 & "', "
+        strSQL = strSQL & "T_EXL_SHIPPINGMEMOLIST.REV_STATUS ='" & str02 & "' "
+        strSQL = strSQL & "WHERE T_EXL_SHIPPINGMEMOLIST.BOOKING_NO ='" & bkgno & "' "
+
+        Command.CommandText = strSQL
+        ' SQLの実行
+        Command.ExecuteNonQuery()
+
+        cnn.Close()
+        cnn.Dispose()
+
+    End Sub
+
+    Private Sub UPD_MEMO03(bkgno As String, str01 As String)
+        '接続文字列の作成
+        Dim ConnectionString As String = String.Empty
+        'SQL Server認証
+        ConnectionString = "Data Source=KBHWPM02;Initial Catalog=EXPDB;User Id=sa;Password=expdb-manager"
+        'SqlConnectionクラスの新しいインスタンスを初期化
+        Dim cnn = New SqlConnection(ConnectionString)
+        Dim Command = cnn.CreateCommand
+        Dim strSQL As String = ""
+        Dim ivno As String = ""
+        Dim dataread As SqlDataReader
+        Dim dbcmd As SqlCommand
+        Dim dataread2 As SqlDataReader
+        Dim dbcmd2 As SqlCommand
+        Dim intCnt As Long
+        Dim dt1 As DateTime = DateTime.Now
+
+        'データベース接続を開く
+        cnn.Open()
+
+        strSQL = ""
+        strSQL = strSQL & "UPDATE T_EXL_SHIPPINGMEMOLIST SET "
+        strSQL = strSQL & "T_EXL_SHIPPINGMEMOLIST.CHECKFLG ='" & str01 & "' "
+        strSQL = strSQL & "WHERE T_EXL_SHIPPINGMEMOLIST.BOOKING_NO ='" & bkgno & "' "
+
+        Command.CommandText = strSQL
+        ' SQLの実行
+        Command.ExecuteNonQuery()
+
+        cnn.Close()
+        cnn.Dispose()
+    End Sub
+
+End Class
