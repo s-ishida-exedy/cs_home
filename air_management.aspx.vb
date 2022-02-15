581bf4e3818b82727372920985553c4e04cc51fa
s-ishida-exedy <s-ishida@exedy.com>
2022-02-15 15:19:28 +0900

    AIR管理表画面追加

---
+++ air_management.aspx.vb
@@ -0,0 +1,290 @@
+﻿Imports System.Data
+Imports System.Data.SqlClient
+Imports System.Linq
+
+Partial Class cs_home
+    Inherits System.Web.UI.Page
+
+    Public strRow As String
+    Public strProcess As String
+
+    Private Sub GridView1_RowCreated(sender As Object, e As System.Web.UI.WebControls.GridViewRowEventArgs) Handles GridView1.RowCreated
+        'ボタンに行数をセット
+        If e.Row.RowType = DataControlRowType.DataRow Then
+            Dim dltButton As Button = e.Row.FindControl("Button1")
+            Dim dltButton2 As Button = e.Row.FindControl("Button2")
+            Dim dltButton3 As Button = e.Row.FindControl("Button3")
+            'ボタンが存在する場合のみセット
+            If Not (dltButton Is Nothing) Then
+                dltButton.CommandArgument = e.Row.RowIndex.ToString()
+                dltButton2.CommandArgument = e.Row.RowIndex.ToString()
+                dltButton3.CommandArgument = e.Row.RowIndex.ToString()
+            End If
+        End If
+
+    End Sub
+
+    Protected Sub GridView1_SelectedIndexChanged(ByVal sender As Object, ByVal e As System.EventArgs) Handles GridView1.SelectedIndexChanged
+        Dim sch_ID As String = GridView1.SelectedValue.ToString
+        '選択された行のSCH_ID
+        strRow = sch_ID
+    End Sub
+
+    Private Sub form1_Load(sender As Object, e As EventArgs) Handles form1.Load
+        If IsPostBack Then
+            ' そうでない時処理
+        Else
+            Me.DropDownList1.Items.Insert(0, "-ETD-") '先頭にタイトル行追加
+            Me.DropDownList2.Items.Insert(0, "-客先-") '先頭にタイトル行追加
+            Me.DropDownList3.Items.Insert(0, "-依頼者-") '先頭にタイトル行追加
+            Me.DropDownList4.Items.Insert(0, "-IVNO-") '先頭にタイトル行追加
+            Me.DropDownList5.Items.Insert(0, "-ETD-") '先頭にタイトル行追加
+            Me.DropDownList6.Items.Insert(0, "-客先-") '先頭にタイトル行追加
+            Me.DropDownList7.Items.Insert(0, "-依頼者-") '先頭にタイトル行追加
+            Me.DropDownList8.Items.Insert(0, "-IVNO-") '先頭にタイトル行追加
+            DropDownList5.Visible = False
+            DropDownList6.Visible = False
+            DropDownList7.Visible = False
+            DropDownList8.Visible = False
+        End If
+
+    End Sub
+
+    Private Sub Make_Grid()
+        'GRIDを作成する。
+        Dim strValue(3) As String
+        Dim Dataobj As New DBAccess
+        Dim strEtd As String = ""
+        Dim strCUST As String = ""
+        Dim strReq As String = ""
+        Dim strIVNO As String = ""
+        Dim strChk As String = "0"
+
+        If CheckBox1.Checked = True Then    '集荷済みも表示
+            strEtd = DropDownList5.SelectedValue
+            strCUST = DropDownList6.SelectedValue
+            strReq = DropDownList7.SelectedValue
+            strIVNO = DropDownList8.SelectedValue
+            strChk = "1"
+        Else
+            strEtd = DropDownList1.SelectedValue
+            strCUST = DropDownList2.SelectedValue
+            strReq = DropDownList3.SelectedValue
+            strIVNO = DropDownList4.SelectedValue
+            strChk = "0"
+        End If
+
+        If strEtd = "-ETD-" Then strEtd = ""
+        If strCUST = "-客先-" Then strCUST = ""
+        If strReq = "-依頼者-" Then strReq = ""
+        If strIVNO = "-IVNO-" Then strIVNO = ""
+
+        '引数の配列へセット
+        strValue = {strEtd, strCUST, strReq, strIVNO}
+
+        'データの取得
+        Dim ds As DataSet = Dataobj.GET_AIR_DATA(strValue, strChk, "0")
+        If ds.Tables.Count > 0 Then
+            GridView1.DataSourceID = ""
+            GridView1.DataSource = ds
+            GridView1.DataBind()
+
+            Dim ds2 As DataSet
+            'ドロップダウンリストのデータバインド
+            If CheckBox1.Checked = True Then    '集荷済みも表示
+                ds2 = Dataobj.GET_AIR_DATA(strValue, strChk, "1")
+                Call DropDown_Ctrl(DropDownList5, ds2, strEtd, "-ETD-", "ETD")
+                ds2 = Dataobj.GET_AIR_DATA(strValue, strChk, "2")
+                Call DropDown_Ctrl(DropDownList6, ds2, strCUST, "-客先-", "CUST_CD")
+                ds2 = Dataobj.GET_AIR_DATA(strValue, strChk, "3")
+                Call DropDown_Ctrl(DropDownList7, ds2, strReq, "-依頼者-", "REQUESTER")
+                ds2 = Dataobj.GET_AIR_DATA(strValue, strChk, "4")
+                Call DropDown_Ctrl(DropDownList8, ds2, strIVNO, "-IVNO-", "IVNO")
+            Else
+                ds2 = Dataobj.GET_AIR_DATA(strValue, strChk, "1")
+                Call DropDown_Ctrl(DropDownList1, ds2, strEtd, "-ETD-", "ETD")
+                ds2 = Dataobj.GET_AIR_DATA(strValue, strChk, "2")
+                Call DropDown_Ctrl(DropDownList2, ds2, strCUST, "-客先-", "CUST_CD")
+                ds2 = Dataobj.GET_AIR_DATA(strValue, strChk, "3")
+                Call DropDown_Ctrl(DropDownList3, ds2, strReq, "-依頼者-", "REQUESTER")
+                ds2 = Dataobj.GET_AIR_DATA(strValue, strChk, "4")
+                Call DropDown_Ctrl(DropDownList4, ds2, strIVNO, "-IVNO-", "IVNO")
+            End If
+
+        End If
+    End Sub
+
+    Private Sub DropDown_Ctrl(objDL As DropDownList, ds As DataSet, strValue As String,
+                              strTitle As String, strKey As String)
+        '検索時のドロップダウンリストのデータセット
+        objDL.Items.Clear()
+        objDL.Items.Insert(0, strTitle) '先頭にタイトル行追加
+        objDL.DataSourceID = ""
+        objDL.DataSource = ds
+        objDL.DataTextField = strKey
+        objDL.DataValueField = strKey
+        objDL.DataBind()
+
+        '選択値の存在チェック後、セット
+        If objDL.Items.FindByValue(strValue) IsNot Nothing And strValue <> "" Then
+            '存在する
+            objDL.SelectedValue = strValue
+        Else
+            objDL.SelectedValue = strTitle
+        End If
+    End Sub
+
+    Private Sub DropDownList1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles DropDownList1.SelectedIndexChanged
+        Call Make_Grid()
+    End Sub
+
+    Private Sub DropDownList2_SelectedIndexChanged(sender As Object, e As EventArgs) Handles DropDownList2.SelectedIndexChanged
+        Call Make_Grid()
+    End Sub
+
+    Private Sub DropDownList3_SelectedIndexChanged(sender As Object, e As EventArgs) Handles DropDownList3.SelectedIndexChanged
+        Call Make_Grid()
+    End Sub
+
+    Private Sub DropDownList4_SelectedIndexChanged(sender As Object, e As EventArgs) Handles DropDownList4.SelectedIndexChanged
+        Call Make_Grid()
+    End Sub
+
+    Private Sub DropDownList5_SelectedIndexChanged(sender As Object, e As EventArgs) Handles DropDownList5.SelectedIndexChanged
+        Call Make_Grid()
+    End Sub
+
+    Private Sub DropDownList6_SelectedIndexChanged(sender As Object, e As EventArgs) Handles DropDownList6.SelectedIndexChanged
+        Call Make_Grid()
+    End Sub
+
+    Private Sub DropDownList7_SelectedIndexChanged(sender As Object, e As EventArgs) Handles DropDownList7.SelectedIndexChanged
+        Call Make_Grid()
+    End Sub
+
+    Private Sub DropDownList8_SelectedIndexChanged(sender As Object, e As EventArgs) Handles DropDownList8.SelectedIndexChanged
+        Call Make_Grid()
+    End Sub
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        'リセットボタン押下
+        DropDownList1.SelectedIndex = 0
+        DropDownList2.SelectedIndex = 0
+        DropDownList3.SelectedIndex = 0
+        DropDownList4.SelectedIndex = 0
+        DropDownList5.SelectedIndex = 0
+        DropDownList6.SelectedIndex = 0
+        DropDownList7.SelectedIndex = 0
+        DropDownList8.SelectedIndex = 0
+        DropDownList1.Visible = True
+        DropDownList2.Visible = True
+        DropDownList3.Visible = True
+        DropDownList4.Visible = True
+
+        DropDownList5.Visible = False
+        DropDownList6.Visible = False
+        DropDownList7.Visible = False
+        DropDownList8.Visible = False
+        CheckBox1.Checked = False
+        Call Make_Grid()
+    End Sub
+
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
+        'チェックボックスON/OFF
+        If CheckBox1.Checked = True Then
+            DropDownList1.Visible = False
+            DropDownList2.Visible = False
+            DropDownList3.Visible = False
+            DropDownList4.Visible = False
+
+            DropDownList5.Visible = True
+            DropDownList6.Visible = True
+            DropDownList7.Visible = True
+            DropDownList8.Visible = True
+        Else
+            DropDownList1.Visible = True
+            DropDownList2.Visible = True
+            DropDownList3.Visible = True
+            DropDownList4.Visible = True
+
+            DropDownList5.Visible = False
+            DropDownList6.Visible = False
+            DropDownList7.Visible = False
+            DropDownList8.Visible = False
+        End If
+
+        '再表示
+        Call Make_Grid()
+
+    End Sub
+
+    Private Sub GridView1_RowCommand(sender As Object, e As GridViewCommandEventArgs) Handles GridView1.RowCommand
+        'グリッド内のボタン押下処理
+        Dim index As Integer = Convert.ToInt32(e.CommandArgument)
+        Dim data1 = Me.GridView1.Rows(index).Cells(5).Text
+        Dim data2 = Me.GridView1.Rows(index).Cells(7).Text
+
+        '更新
+        If e.CommandName = "doc_fin" Then
+            Call Update_FLG("doc_fin", data1, data2)
+        ElseIf e.CommandName = "pic_fin" Then
+            Call Update_FLG("pic_fin", data1, data2)
+        ElseIf e.CommandName = "update" Then
+            '入力された客先コードをセッションへ
+            Session("strEtd") = data1
+            Session("strIvno") = data2
+            Session("strMode") = "01"       '更新モード
+
+            '画面遷移
+            Response.Redirect("air_mng_detail.aspx")
+            Return
+        End If
+
+        '画面再表示
+        Call Make_Grid()
+    End Sub
+
+    Private Sub Update_FLG(strFlg As String, strEtd As String, strIvno As String)
+        'レコードのフラグを取得する。
+        Dim strSQL As String
+        Dim dtNow As DateTime = DateTime.Now
+
+        '接続文字列の作成
+        Dim ConnectionString As String = String.Empty
+        'SQL Server認証
+        ConnectionString = "Data Source=kbhwpm02;Initial Catalog=EXPDB;User Id=sa;Password=expdb-manager"
+        'SqlConnectionクラスの新しいインスタンスを初期化
+        Dim cnn = New SqlConnection(ConnectionString)
+        Dim Command = cnn.CreateCommand
+        Dim i As Integer = 0
+
+        'データベース接続を開く
+        cnn.Open()
+
+        strSQL = ""
+        'フラグをUPDATE
+        If strFlg = "doc_fin" Then
+            strSQL = strSQL & "UPDATE T_EXL_AIR_MANAGE SET DOC_FIN = '作成済み' "
+            strSQL = strSQL & "WHERE ETD = '" & strEtd & "' AND IVNO = '" & strIvno & "' "
+        ElseIf strFlg = "pic_fin" Then
+            strSQL = strSQL & "UPDATE T_EXL_AIR_MANAGE SET PICKUP = '集荷済み' "
+            strSQL = strSQL & "WHERE ETD = '" & strEtd & "' AND IVNO = '" & strIvno & "' "
+        End If
+
+        Command.CommandText = strSQL
+        ' SQLの実行
+        Command.ExecuteNonQuery()
+
+        cnn.Close()
+        cnn.Dispose()
+
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        '新規登録ボタン押下
+        Session("strMode") = "02"       '更新モード
+
+        '画面遷移
+        Response.Redirect("air_mng_detail.aspx")
+
+    End Sub
+End Class
---
+++ air_mng_detail.aspx.vb
@@ -0,0 +1,260 @@
+﻿Imports System.Data.SqlClient
+Imports mod_function
+
+Partial Class cs_home
+    Inherits System.Web.UI.Page
+
+    Private Sub form1_Load(sender As Object, e As EventArgs) Handles form1.Load
+        Dim strSQL As String = ""
+        Dim dataread As SqlDataReader
+        Dim dbcmd As SqlCommand
+        Dim strValue As String = ""
+        Dim strVal As String = ""
+
+        Label1.Text = ""
+
+        If IsPostBack Then
+            ' そうでない時処理
+        Else
+            'パラメータ取得
+            Dim strEtd As String = Session("strEtd")
+            Dim strIvno As String = Session("strIvno")
+            Dim strMode As String = Session("strMode")
+
+            If strMode = "01" Then
+
+                '接続文字列の作成
+                Dim ConnectionString As String = String.Empty
+                'SQL Server認証
+                ConnectionString = "Data Source=kbhwpm02;Initial Catalog=EXPDB;User Id=sa;Password=expdb-manager"
+                'SqlConnectionクラスの新しいインスタンスを初期化
+                Dim cnn = New SqlConnection(ConnectionString)
+
+                'データベース接続を開く
+                cnn.Open()
+
+                strSQL = ""
+                strSQL = strSQL & "SELECT * FROM T_EXL_AIR_MANAGE "
+                strSQL = strSQL & "WHERE ETD = '" & strEtd & "' "
+                strSQL = strSQL & "AND   IVNO = '" & strIvno & "' "
+
+                'ＳＱＬコマンド作成 
+                dbcmd = New SqlCommand(strSQL, cnn)
+                'ＳＱＬ文実行 
+                dataread = dbcmd.ExecuteReader()
+
+                '結果を取り出す 
+                While (dataread.Read())
+                    DropDownList1.SelectedValue = dataread("DOC_FIN").ToString
+                    DropDownList2.SelectedValue = dataread("PICKUP").ToString
+                    DropDownList3.SelectedValue = dataread("PLACE").ToString
+
+                    TextBox1.Text = dataread("REQUESTED_DATE").ToString
+                    TextBox2.Text = dataread("CREATED_DATE").ToString
+                    TextBox3.Text = dataread("ETD").ToString
+                    TextBox4.Text = dataread("CUST_CD").ToString
+                    TextBox5.Text = dataread("IVNO").ToString
+                    TextBox6.Text = dataread("REQUESTER").ToString
+                    TextBox7.Text = dataread("DEPARTMENT").ToString
+                    TextBox8.Text = dataread("AUTHOR").ToString
+                    TextBox9.Text = dataread("SHIPPING_COMPANY").ToString
+                    TextBox10.Text = dataread("REMARKS").ToString
+                End While
+
+                'クローズ処理 
+                dataread.Close()
+                dbcmd.Dispose()
+                cnn.Close()
+                cnn.Dispose()
+            End If
+
+            'モードによりボタン名称を変更する
+            If strMode = "01" Then
+                Button1.Text = "更　　新"
+                Button1.Attributes.Add("onclick", "return confirm('更新します。よろしいですか？');")
+            Else
+                Button1.Text = "登　　録"
+                Button1.Attributes.Add("onclick", "return confirm('登録します。よろしいですか？');")
+            End If
+
+        End If
+
+    End Sub
+
+    Private Sub DB_access()
+        '画面入力情報をテーブルへ登録
+        Dim strSQL As String
+        Dim strVal As String = ""
+
+        'パラメータ取得
+        Dim strEtd As String = Session("strEtd")
+        Dim strIvno As String = Session("strIvno")
+        Dim strMode As String = Session("strMode")
+
+        '接続文字列の作成
+        Dim ConnectionString As String = String.Empty
+        'SQL Server認証
+        ConnectionString = "Data Source=KBHWPM02;Initial Catalog=EXPDB;User Id=sa;Password=expdb-manager"
+        'SqlConnectionクラスの新しいインスタンスを初期化
+        Dim cnn = New SqlConnection(ConnectionString)
+        Dim Command = cnn.CreateCommand
+
+        'データベース接続を開く
+        cnn.Open()
+
+        If strMode = "01" Then
+            'データ更新
+            strSQL = ""
+            strSQL = strSQL & "UPDATE T_EXL_AIR_MANAGE "
+            strSQL = strSQL & "SET "
+            strSQL = strSQL & "  REQUESTED_DATE =  '" & LTrim(RTrim(TextBox1.Text)) & "' "
+            strSQL = strSQL & "  , CREATED_DATE =  '" & LTrim(RTrim(TextBox2.Text)) & "' "
+            strSQL = strSQL & "  , ETD =  '" & LTrim(RTrim(TextBox3.Text)) & "' "
+            strSQL = strSQL & "  , CUST_CD =  '" & LTrim(RTrim(TextBox4.Text)) & "' "
+            strSQL = strSQL & "  , IVNO =  '" & LTrim(RTrim(TextBox5.Text)) & "' "
+            strSQL = strSQL & "  , REQUESTER =  '" & LTrim(RTrim(TextBox6.Text)) & "' "
+            strSQL = strSQL & "  , DEPARTMENT =  '" & LTrim(RTrim(TextBox7.Text)) & "' "
+            strSQL = strSQL & "  , AUTHOR =  '" & LTrim(RTrim(TextBox8.Text)) & "' "
+            strSQL = strSQL & "  , DOC_FIN =  '" & DropDownList1.SelectedValue & "' "
+            strSQL = strSQL & "  , SHIPPING_COMPANY =  '" & LTrim(RTrim(TextBox9.Text)) & "' "
+            strSQL = strSQL & "  , PICKUP =  '" & DropDownList2.SelectedValue & "' "
+            strSQL = strSQL & "  , PLACE =  '" & DropDownList3.SelectedValue & "' "
+            strSQL = strSQL & "  , REMARKS =  '" & LTrim(RTrim(TextBox10.Text)) & "' "
+            strSQL = strSQL & "WHERE "
+            strSQL = strSQL & "       ETD =  '" & strEtd & "' "
+            strSQL = strSQL & "  AND IVNO =  '" & strIvno & "' "
+        Else
+            strSQL = ""
+            strSQL = strSQL & "INSERT INTO T_EXL_AIR_MANAGE "
+            strSQL = strSQL & "VALUES ( "
+            strSQL = strSQL & "    '" & LTrim(RTrim(TextBox1.Text)) & "' "
+            strSQL = strSQL & "  , '" & LTrim(RTrim(TextBox2.Text)) & "' "
+            strSQL = strSQL & "  , '" & LTrim(RTrim(TextBox3.Text)) & "' "
+            strSQL = strSQL & "  , '" & LTrim(RTrim(TextBox4.Text)) & "' "
+            strSQL = strSQL & "  , '" & LTrim(RTrim(TextBox5.Text)) & "' "
+            strSQL = strSQL & "  , '" & LTrim(RTrim(TextBox6.Text)) & "' "
+            strSQL = strSQL & "  , '" & LTrim(RTrim(TextBox7.Text)) & "' "
+            strSQL = strSQL & "  , '" & LTrim(RTrim(TextBox8.Text)) & "' "
+            strSQL = strSQL & "  , '" & DropDownList1.SelectedValue & "' "
+            strSQL = strSQL & "  , '" & LTrim(RTrim(TextBox9.Text)) & "' "
+            strSQL = strSQL & "  , '" & DropDownList2.SelectedValue & "' "
+            strSQL = strSQL & "  , '" & DropDownList3.SelectedValue & "' "
+            strSQL = strSQL & "  , '" & LTrim(RTrim(TextBox10.Text)) & "' "
+            strSQL = strSQL & ") "
+        End If
+
+        Command.CommandText = strSQL
+        ' SQLの実行
+        Command.ExecuteNonQuery()
+
+        cnn.Close()
+
+    End Sub
+
+    Private Function chk_Nyuryoku() As Boolean
+        chk_Nyuryoku = True
+
+        '必須チェック
+        If Trim(TextBox1.Text) = "" Then
+            Label1.Text = "依頼日は必須入力です。"
+            chk_Nyuryoku = False
+        End If
+        If Trim(TextBox2.Text) = "" Then
+            Label1.Text = "作成日は必須入力です。"
+            chk_Nyuryoku = False
+        End If
+        If Trim(TextBox3.Text) = "" Then
+            Label1.Text = "ETDは必須入力です。"
+            chk_Nyuryoku = False
+        End If
+        If Trim(TextBox4.Text) = "" Then
+            Label1.Text = "客先コードは必須入力です。"
+            chk_Nyuryoku = False
+        End If
+
+        '全角入力チェック
+        If HankakuEisuChk(TextBox4.Text) = False And Trim(TextBox4.Text) <> "" Then
+            Label1.Text = "客先コードに全角文字が使用されています。"
+            chk_Nyuryoku = False
+        End If
+        If HankakuEisuChk(TextBox5.Text) = False And Trim(TextBox5.Text) <> "" Then
+            Label1.Text = "IVNOに全角文字が使用されています。"
+            chk_Nyuryoku = False
+        End If
+
+        '桁数チェック
+        If Len(TextBox4.Text) <> 4 Then
+            Label1.Text = "客先コードは４桁で入力してください。"
+            chk_Nyuryoku = False
+        End If
+        If Len(TextBox5.Text) <> 4 Then
+            Label1.Text = "IVNOは４桁で入力してください。"
+            chk_Nyuryoku = False
+        End If
+
+    End Function
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        '更新（登録）ボタンクリックイベント
+        Dim dbcmd As SqlCommand
+        Dim dataread As SqlDataReader
+        Dim StrSQL As String = ""
+
+        '入力チェック
+        If chk_Nyuryoku() = False Then
+            Return
+        End If
+
+        'パラメータ取得
+        Dim strMode As String = Session("strMode")
+
+        '登録時、存在チェック
+        If strMode = "02" Then
+            '接続文字列の作成
+            Dim ConnectionString As String = String.Empty
+            'SQL Server認証
+            ConnectionString = "Data Source=KBHWPM02;Initial Catalog=EXPDB;User Id=sa;Password=expdb-manager"
+            'SqlConnectionクラスの新しいインスタンスを初期化
+            Dim cnn = New SqlConnection(ConnectionString)
+            Dim Command = cnn.CreateCommand
+
+            'データベース接続を開く
+            cnn.Open()
+
+            StrSQL = ""
+            StrSQL = StrSQL & "SELECT COUNT(*) AS RecCnt FROM T_EXL_AIR_MANAGE "
+            StrSQL = StrSQL & "WHERE ETD = '" & Trim(TextBox3.Text) & "'"
+            StrSQL = StrSQL & " AND IVNO = '" & Trim(TextBox5.Text) & "'"
+
+            'ＳＱＬコマンド作成 
+            dbcmd = New SqlCommand(StrSQL, cnn)
+            'ＳＱＬ文実行 
+            dataread = dbcmd.ExecuteReader()
+
+            '結果を取り出す 
+            Dim intCnt As Integer = 0
+            While (dataread.Read())
+                intCnt = dataread("RecCnt")
+            End While
+
+            'クローズ処理 
+            dataread.Close()
+            dbcmd.Dispose()
+            cnn.Close()
+            cnn.Dispose()
+
+            If intCnt > 0 Then
+                Label1.Text = "ETD:" & Trim(TextBox1.Text) & "、IVNO:" & Trim(TextBox5.Text) & "は既に登録済みです。"
+                Return
+            End If
+        End If
+
+        '更新(登録)
+        Call DB_access()
+
+        '元の画面に戻る
+        Response.Redirect("air_management.aspx")
+    End Sub
+End Class
+
