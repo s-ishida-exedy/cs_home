ff32dbf1b610c8f5ac02403f71f17b82b982d220
exedy-fukao <r-fukao@exedy.com>
2022-02-28 12:16:25 +0900

    ファイル開く

--- register_csstatus.aspx.vb
+++ register_csstatus.aspx.vb
@@ -648,6 +648,8 @@ Partial Class yuusen
 
     Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
 
+
+        Response.Redirect("登録用_カレンダマスタ.xls")
         'Dim p As New System.Diagnostics.Process
         'p.StartInfo.FileName = “\\kbhwpm01\exp\登録用_T_EXL_CSWORKSTATUS.xls”
         'p.Start()
