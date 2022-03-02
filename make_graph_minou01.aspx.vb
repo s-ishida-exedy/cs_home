acbf53c333edb6ba388292c319b805403f636d41
exedy-fukao <r-fukao@exedy.com>
2022-03-02 15:35:11 +0900

    合計追加

--- make_graph_minou01.aspx.vb
+++ make_graph_minou01.aspx.vb
@@ -14,6 +14,9 @@ Imports System.Web
 Imports System.Web.UI
 Imports System.Web.UI.WebControls
 
+Imports System.Console
+Imports ClosedXML.Excel
+
 
 
 
@@ -46,7 +49,7 @@ Partial Class yuusen
         Dim arrColor As String() = New String() {"#231F20", "#FFC200", "#F44937", "#16F27E", "#FC9775", "#5A69A6"}
 
         Using cn As SqlConnection = New SqlConnection("Data Source=kbhwpm02;Initial Catalog=EXPDB;User Id=sa;Password=expdb-manager")
-            Dim myQuery As String = "select CUSTCODE,MINOU,RED,DELAY,ADJ,ACM from T_EXL_GRAPH_MINOU_AM "
+            Dim myQuery As String = "select CUSTCODE,MINOU,RED,DELAY,ADJ,ACM from T_EXL_GRAPH_MINOU_AM WHERE CUSTCODE <>'' AND CUSTCODE <>'合計' "
             Dim cmd As SqlCommand = New SqlCommand()
             cmd.CommandText = myQuery
             cmd.CommandType = CommandType.Text
@@ -77,6 +80,63 @@ Partial Class yuusen
 
         Return t
     End Function
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim dt As New DataTable("GridView_Data")
+        For Each cell As TableCell In GridView1.HeaderRow.Cells
+            dt.Columns.Add(cell.Text)
+        Next
+        For Each row As GridViewRow In GridView1.Rows
+            dt.Rows.Add()
+            For i As Integer = 0 To row.Cells.Count - 1
+                If i = 0 Then
+                    dt.Rows(dt.Rows.Count - 1)(i) = row.Cells(i).Text
+
+                Else
+
+                    dt.Rows(dt.Rows.Count - 1)(i) = Double.Parse(Replace(row.Cells(i).Text, "&nbsp;", 0)).ToString("#,0")
+
+                End If
+            Next
+        Next
+        Using wb As New XLWorkbook()
+            wb.Worksheets.Add(dt)
+            wb.SaveAs("C:\Users\T43529\OneDrive - 株式会社エクセディ\デスクトップ\新ツール\未納_アフタ_" & Now.ToString(“yyyyMMddhh”) & ".xlsx")
+
+        End Using
+
+
+    End Sub
+
+    Private Sub GridView1_RowCreated(sender As Object, e As System.Web.UI.WebControls.GridViewRowEventArgs) Handles GridView1.RowDataBound
+
+
+        If e.Row.RowType = DataControlRowType.DataRow Then
+
+            If e.Row.Cells(1).Text <> "&nbsp;" Then
+
+
+
+                e.Row.Cells(1).Text = Double.Parse(e.Row.Cells(1).Text).ToString("#,0.0")
+                e.Row.Cells(2).Text = Double.Parse(e.Row.Cells(2).Text).ToString("#,0.0")
+                e.Row.Cells(3).Text = Double.Parse(e.Row.Cells(3).Text).ToString("#,0.0")
+                e.Row.Cells(4).Text = Double.Parse(e.Row.Cells(4).Text).ToString("#,0.0")
+                e.Row.Cells(5).Text = Double.Parse(e.Row.Cells(5).Text).ToString("#,0.0")
+
+
+
+
+
+            End If
+
+        End If
+
+    End Sub
+
+
+
+
 
 
 End Class
