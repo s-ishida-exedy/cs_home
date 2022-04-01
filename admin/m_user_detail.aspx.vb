de1cf06e6d25577c4b8fe82cd3f85aad5b9f1ab4
s-ishida-exedy <s-ishida@exedy.com>
2022-04-01 09:43:34 +0900

    ラジオボタン単一選択の制御が入っていなかったので追加

--- admin/m_user_detail.aspx.vb
+++ admin/m_user_detail.aspx.vb
@@ -183,5 +183,27 @@ Partial Class cs_home
         '元の画面に戻る
         Response.Redirect("m_user.aspx")
     End Sub
+
+    Private Sub RadioButton1_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton1.CheckedChanged
+        If RadioButton1.Checked = True Then
+            RadioButton2.Checked = False
+            RadioButton3.Checked = False
+        End If
+    End Sub
+
+    Private Sub RadioButton2_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton2.CheckedChanged
+        If RadioButton2.Checked = True Then
+            RadioButton1.Checked = False
+            RadioButton3.Checked = False
+        End If
+    End Sub
+
+    Private Sub RadioButton3_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton3.CheckedChanged
+        If RadioButton3.Checked = True Then
+            RadioButton1.Checked = False
+            RadioButton2.Checked = False
+        End If
+
+    End Sub
 End Class
 
