af41764ef658d547b0f5c6c94ec712c5e8deb59a
s-ishida-exedy <s-ishida@exedy.com>
2022-06-17 08:37:51 +0900

    サイドのトピックス表示条件をホームと合わせる変更

--- topics_detail.aspx.vb
+++ topics_detail.aspx.vb
@@ -123,6 +123,8 @@ Partial Class cs_home
         strSQL = strSQL & "SELECT AA.* "
         strSQL = strSQL & "FROM (SELECT TOP 5 *   "
         strSQL = strSQL & "FROM T_EXL_TOPICS  "
+        strSQL = strSQL & "WHERE INFO_DATE > DATEADD(DAY, -30, GETDATE()) "
+        strSQL = strSQL & "AND   FIN_FLG = '0' "
         strSQL = strSQL & "ORDER BY INFO_DATE DESC, INFO_TIME DESC) AA "
         strSQL = strSQL & "ORDER BY INFO_DATE , INFO_TIME "
 
