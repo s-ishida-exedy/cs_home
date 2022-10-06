62f4d2e4300d01876c2c031b951a1c61dfc33b73
s-ishida-exedy <s-ishida@exedy.com>
2022-10-07 08:41:50 +0900

    T_EXL_VAN_SCH_DETAILのデータ取得時、客先の後方に空白が入っていたのでTRIMで削除

--- start.aspx.vb
+++ start.aspx.vb
@@ -121,16 +121,16 @@ Partial Class cs_home
         While (dataread.Read())
             Select Case dataread("PLACE")
                 Case "0H"
-                    If strTody = dataread("VAN_DATE") And Left(Right(dataread("CUST_NM"), 5), 1) = "K" Then
+                    If strTody = dataread("VAN_DATE") And Left(Right(Trim(dataread("CUST_NM")), 5), 1) = "K" Then
                         '当日　本社　AF
                         lngHAf += 1
-                    ElseIf strTody = dataread("VAN_DATE") And Left(Right(dataread("CUST_NM"), 5), 1) <> "K" Then
+                    ElseIf strTody = dataread("VAN_DATE") And Left(Right(trim(dataread("CUST_NM")), 5), 1) <> "K" Then
                         '当日　本社　KD
                         lngHKd += 1
-                    ElseIf strTomm = dataread("VAN_DATE") And Left(Right(dataread("CUST_NM"), 5), 1) = "K" Then
+                    ElseIf strTomm = dataread("VAN_DATE") And Left(Right(trim(dataread("CUST_NM")), 5), 1) = "K" Then
                         '翌日　本社　AF
                         lngHAf2 += 1
-                    ElseIf strTomm = dataread("VAN_DATE") And Left(Right(dataread("CUST_NM"), 5), 1) <> "K" Then
+                    ElseIf strTomm = dataread("VAN_DATE") And Left(Right(trim(dataread("CUST_NM")), 5), 1) <> "K" Then
                         '翌日　本社　KD
                         lngHKd2 += 1
                     End If
